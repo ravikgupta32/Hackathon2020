66156ca9371f04ea47310aca46364a1498d6855b
Ravi <ravikgupta32@gmail.com>
2020-10-18 12:02:12 +0530

    adding files

---
+++ Dashboard/app/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='1.0',
+    packages=['package'],
+    url='',
+    license='',
+    author='Coder Champs',
+    author_email='hospitalmanagment20@gmail.com@gmail.com',
+    description='HospitalManagementSystem for hackathon'
+)
